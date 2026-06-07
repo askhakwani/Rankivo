4c555e4f345ed827e3115c1a1961a2c9da9c0237
Ayesha <askhakwani@gmail.com>
2026-06-08 02:44:48 +0500

    fix metas

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,8 +1,5 @@
 'use client'
 import { useState } from 'react'
-...
-'use client'
-import { useState } from 'react'
 import Navbar from '../../components/Navbar'
 import Footer from '../../components/Footer'
 
